4a3c48273af25ab3b4b9f4783245b5f1b0fd4cfd
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-05-30 16:08:22 +0500

    question2 added

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 
 // var currentDate = new Date()
 // console.log(currentDate)
+
+
+// **********QUESTION 2**********
+
+// var monthName = ["January", "Febrary", "March", "April", "May", "June", "July", "August",
+//     "September", "October", "November", "December"]
+
+// var date = new Date()
+// console.log("The current Month is " + monthName[date.getMonth()])
